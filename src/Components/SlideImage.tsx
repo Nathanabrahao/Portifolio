23c30a63dc04ec7434ed1c8a2cb999dab7f6d7ea
Nathanabrahao <nathanntc6@gmail.com>
2024-04-05 13:33:03 -0300

    Update icon page

--- src/Components/SlideImage.tsx
+++ src/Components/SlideImage.tsx
@@ -12,12 +12,26 @@ export function ImageSlider({ imageUrls }: ImageSliderProps) {
     const [imageIndex, setImageIndex] = useState(0)
 
 
-    return <div style={{ width: "100%", height: "100%" }}>
+    function showNextImage(){
+        setImageIndex(index => {
+            if(index === imageUrls.length - 1) return 0
+            return index + 1
+        })
+    }
+
+    function showPrevImage(){
+        setImageIndex(index => {
+            if (index === 0) return imageUrls.length - 1
+            return index - 1
+        })  
+    }
+
+    return <div style={{ width: "100%", height: "100%", position: "relative" }}>
         <img src={imageUrls[imageIndex]} className='Nathan_picture' />
-        <button>
+        <button onClick={showPrevImage} className='img-slider-btn' style={{ left: 0 }}>
             <ArrowBigLeft />
         </button>
-        <button>
+        <button onClick={showNextImage} className='img-slider-btn' style={{ right: 0 }}>
             <ArrowBigRight />
         </button>
     </div>
--- src/Pages/AboutMe/AboutMe.tsx
+++ src/Pages/AboutMe/AboutMe.tsx
@@ -24,30 +24,37 @@ const AboutMe = (props: Props) => {
                 <div><h1 className='About__Title'>
                     Who it's me?
                 </h1>
-                    <p>
-                        I'm front end developer, gamer, reader and streamer.
-                        <br></br>
-                        I'm like so much all games for to <a
-                            className="Resident_Evil"
-                            href="https://pt.wikipedia.org/wiki/Resident_Evil"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            Resident Evil ,
-                        </a>
-                        my favorite book for this moment is
-                        <a
-                            className="A_Sutil_Arte_de_ligar_o_fodasse"
-                            href="https://en.wikipedia.org/wiki/The_Subtle_Art_of_Not_Giving_a_F*ck"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            "The subtle art of not giving a f#ck"
-                        </a>
-                        <br></br>
-                        I'm go to migration my knoledge to Front End knoledge, this because my experience it's about SQL and another things, but this i show you im my
-                        <a className='Experience__aboutMe' href='Experience'> Experience</a>
-                    </p>
+                    <div>
+                        <p>
+                            My name is Nathan Bittencourt Abdon Abrahão, i have 23 years old and i'm brazilian
+                            <br></br>
+                            I'm front end developer, gamer, reader and streamer.
+                        </p>
+                    </div>
+                    <div>
+                        <p>
+                            I'm like so much all games for to <a
+                                className="Resident_Evil"
+                                href="https://pt.wikipedia.org/wiki/Resident_Evil"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Resident Evil ,
+                            </a>
+                            my favorite book for this moment is
+                            <a
+                                className="A_Sutil_Arte_de_ligar_o_fodasse"
+                                href="https://en.wikipedia.org/wiki/The_Subtle_Art_of_Not_Giving_a_F*ck"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                "The subtle art of not giving a f#ck"
+                            </a>
+                            <br></br>
+                            I'm go to migration my knoledge to Front End knoledge, this because my experience it's about SQL and another things, but this i show you im my
+                            <a className='Experience__aboutMe' href='Experience'> Experience</a>
+                        </p>
+                    </div>
                 </div>
                 <div
                     style={{
@@ -64,4 +71,3 @@ const AboutMe = (props: Props) => {
 }
 
 export default AboutMe;
-
