e43215b034e9f02a1ed3a1a1ebde96783843d4ea
Nathanabrahao <nathanntc6@gmail.com>
2024-05-03 10:43:41 -0300

    Add web player project

--- src/Components/GithubProjects.tsx
+++ src/Components/GithubProjects.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { SocialIcon } from "react-social-icons"
 import recognize from "../../src/Images/recognize_number.png"
 import table from "../../src/Images/table_angular.png"
-import flapy from "../../src/Images/Flappy Bird.png"
+import webPlayer from "../../src/Images/Web_Player.png"
 import "../Pages/Projects/Projects.css"
 
 type Project = {
@@ -18,12 +18,12 @@ type ProjectsGit = {
 };
 
 export const projectsGit: ProjectsGit = {
-    recognize: {
-        name: "recognize_number",
-        url: "https://recognize-number-game.vercel.app/",
-        label: "recognize",
-        img: recognize,
-        text: "In this game, you speak a number, and the site responds with the correct number or validates if you are closer to the correct number."
+    Web_player: {
+        name: "Web Player",
+        url: "https://web-player-music.vercel.app/",
+        label: "Web_player",
+        img: webPlayer,
+        text: "This is a web player project, where all the songs is created with Udio(IA creator song), this project show web player active and songs created with my creativity"
     },
     Tables_Angular: {
         name: "tables_angular",
@@ -31,5 +31,12 @@ export const projectsGit: ProjectsGit = {
         label: "Angular_users",
         img: table,
         text: "This is a 24-hour challenge. I need to create a table where I can read, write, update, and delete users and tasks. I will be completing this challenge using Angular."
+    },
+    recognize: {
+        name: "recognize_number",
+        url: "https://recognize-number-game.vercel.app/",
+        label: "recognize",
+        img: recognize,
+        text: "In this game, you speak a number, and the site responds with the correct number or validates if you are closer to the correct number."
     }
 };
