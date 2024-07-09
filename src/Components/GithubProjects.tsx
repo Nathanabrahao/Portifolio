d24bccdbf4bdc9460c18f3e1b1f44545c7208beb
Nathan_Abrahão <54583223+Nathanabrahao@users.noreply.github.com>
2024-07-09 11:21:33 -0300

    Update GithubProjects.tsx

--- src/Components/GithubProjects.tsx
+++ src/Components/GithubProjects.tsx
@@ -33,13 +33,13 @@ export const projectsGit: ProjectsGit = {
         img: Rtodo,
         text: "In this project, you created, edit and delete, tasks where you subject the name and the difficulty to realized"
     },
-    Tables_Angular: {
-        name: "tables_angular",
-        url: "https://tables-users.vercel.app/criaUsuario",
-        label: "Angular_users",
-        img: table,
-        text: "This is a 24-hour challenge. I need to create a table where I can read, write, update, and delete users and tasks. I will be completing this challenge using Angular."
-    },
+//    Tables_Angular: {
+//        name: "tables_angular",
+//        url: "https://tables-users.vercel.app/criaUsuario",
+//        label: "Angular_users",
+//        img: table,
+//        text: "This is a 24-hour challenge. I need to create a table where I can read, write, update, and delete users and tasks. I will be completing this challenge using Angular."
+//    },
     recognize: {
         name: "recognize_number",
         url: "https://recognize-number-game.vercel.app/",
